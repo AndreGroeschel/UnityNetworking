1cd9e86778e2a1ec5c4d2cac3d259b3de9392c06
Andre Groeschel <andre.groeschel@gmail.com>
2015-06-14 21:45:18 +0200

    Initial commit

---
+++ Assets/Scripts/Network/PlayerHeadRotation.cs
@@ -0,0 +1,105 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Andre Groeschel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+namespace Kontraproduktiv
+{
+    /// <summary>
+    /// Syncronizes the player's head orientation over the network
+    /// </summary>
+    public class PlayerHeadRotation : NetworkBehaviour 
+	{
+        #region MEMBER VARIABLES
+        [SyncVar]
+        private Quaternion m_SyncPlayerRotation;
+
+        [SerializeField]
+        private Transform m_HeadTransform;
+
+        [SerializeField]
+        private Transform m_PlayerCamera;
+
+        [SerializeField]
+        private float m_SmoothingFactor = 15f;
+
+        #endregion
+
+        #region UNITY FUNCTIONS
+
+        void Start ()
+		{
+            if (m_HeadTransform == null)
+            {
+                m_HeadTransform = transform;
+                
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (isLocalPlayer == true)
+            {
+                // transmit rotation to server
+                TransmitRotation();
+            }
+            // interpolate head rotations of other players
+            else
+            {
+                InterpolateRotation();
+            }
+        }
+
+        #endregion
+
+        #region METHODS
+        private void InterpolateRotation()
+        {
+            m_HeadTransform.rotation = Quaternion.Lerp(m_HeadTransform.rotation, m_SyncPlayerRotation, Time.deltaTime * m_SmoothingFactor);
+        }
+        #endregion
+
+        #region NETWORKING METHODS
+        [Command]
+        void CmdProvideRotationToServer(Quaternion in_Rotation)
+        {
+            m_SyncPlayerRotation = in_Rotation;
+        }
+
+        [ClientCallback]
+        private void TransmitRotation()
+        {
+            CmdProvideRotationToServer(m_PlayerCamera.rotation);
+        }
+        #endregion
+
+        #region EVENT HANDLER
+        #endregion
+
+        #region PROPERTIES
+        #endregion
+
+    }
+}
---
+++ Assets/Scripts/Network/PlayerPositionSync.cs
@@ -0,0 +1,105 @@
+﻿/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Andre Groeschel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+namespace Kontraproduktiv
+{
+	/// <summary>
+	/// Syncronizes the player's postion over the network
+	/// </summary>
+	public class PlayerPositionSync : NetworkBehaviour 
+	{
+		#region MEMBER VARIABLES
+		[SyncVar]
+		private Vector3 m_SyncPlayerPostion;
+
+		[SerializeField]
+		private Transform m_PlayerTransform;
+
+		[SerializeField]
+		private float m_SmoothingFactor;
+        #endregion
+
+        #region UNITY FUNCTIONS
+
+        void Start ()
+		{
+			if(m_PlayerTransform == null)
+				m_PlayerTransform = transform;
+		}
+		
+		void FixedUpdate ()
+		{		
+			if(isLocalPlayer == true)
+			{
+				// transmit position to server
+				TransmitPosition();
+
+                // transmit rotation to server
+                //TransmitRotation();
+            }
+			
+			else
+			{
+                // interpolate positions of other players
+                InterpolatePostion();
+
+            }
+		}
+        #endregion
+
+
+        #region METHODS
+        private void InterpolatePostion()
+        {
+
+            m_PlayerTransform.position = Vector3.Lerp(m_PlayerTransform.position, m_SyncPlayerPostion, Time.deltaTime * m_SmoothingFactor);
+        }
+        #endregion
+
+        #region NETWORKING METHODS
+        [Command]
+		void CmdProvidePositionToServer(Vector3 in_Position)
+		{
+			m_SyncPlayerPostion = in_Position;
+		}
+
+
+        [ClientCallback]
+		private void TransmitPosition()
+		{
+			CmdProvidePositionToServer(m_PlayerTransform.position);
+		}
+        #endregion
+
+        #region EVENT HANDLER
+        #endregion
+
+        #region PROPERTIES
+        #endregion
+
+    }
+}
